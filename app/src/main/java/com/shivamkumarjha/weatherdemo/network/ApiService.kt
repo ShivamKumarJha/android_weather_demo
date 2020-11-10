014429ad8fd1cee49eda57bc01711229a5706bee
ShivamKumarJha <jha.shivam3@gmail.com>
2020-11-10 15:50:25 +0530

    Api call functions
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/weatherdemo/network/ApiService.kt
+++ app/src/main/java/com/shivamkumarjha/weatherdemo/network/ApiService.kt
@@ -1,6 +1,10 @@
 package com.shivamkumarjha.weatherdemo.network
 
+import com.shivamkumarjha.weatherdemo.model.WeatherMain
+import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
@@ -15,4 +19,16 @@ interface ApiService {
             return INSTANCE!!
         }
     }
+
+    @GET("weather")
+    fun getWeather(
+        @Query("q") q: String,
+        @Query("APPID") APPID: String
+    ): Call<WeatherMain>
+
+    @GET("forecast")
+    fun getForecast(
+        @Query("q") q: String,
+        @Query("APPID") APPID: String
+    ): Call<WeatherMain>
 }
