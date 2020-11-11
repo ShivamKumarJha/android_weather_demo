c7cc94d5ae863ac68b40c1399327aed3f8cb9cb4
ShivamKumarJha <jha.shivam3@gmail.com>
2020-11-11 18:18:16 +0530

    Add UI espresso tests
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/androidTest/java/com/shivamkumarjha/weatherdemo/MainActivityTest.kt
@@ -0,0 +1,33 @@
+package com.shivamkumarjha.weatherdemo
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.shivamkumarjha.weatherdemo.ui.main.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun appLaunchesSuccessfully() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun onLaunchCheckIsProgressBarDisplayed() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.progress_bar_id))
+            .check(matches(isDisplayed()))
+    }
+}
